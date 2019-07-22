568dee704070eb8e7a1ee0864eefde72e40e7cd5
Dang <dht925nerd@126.com>
2019-07-22 14:12:55 +0800

    虚拟机栈和本地方法栈OOM测试

---
+++ JVM/src/me/dang/chapter02/JavaVMStackSOF.java
@@ -0,0 +1,28 @@
+package me.dang.chapter02;
+
+/**
+ * 如果线程请求的栈深度大于虚拟机所允许的最大深度，将抛出StackOverflowError异常
+ * VM Args: -Xss128k
+ * @author dht
+ * @date 17/07/2019
+ */
+public class JavaVMStackSOF {
+
+    private int stackLength = 1;
+
+    public void stackLeak() {
+        stackLength++;
+        stackLeak();
+    }
+
+    public static void main(String[] args) throws Throwable {
+        JavaVMStackSOF oom = new JavaVMStackSOF();
+        try {
+            oom.stackLeak();
+        } catch (Throwable e) {
+            System.out.println("stack length:" + oom.stackLength);
+            throw e;
+        }
+    }
+
+}
