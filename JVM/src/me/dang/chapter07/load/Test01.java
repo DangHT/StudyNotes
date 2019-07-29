59ca5ca5738bafcd909950af034db77db0846158
Dang <dht925nerd@126.com>
2019-07-29 15:09:06 +0800

    通过子类引用父类静态字段，不会导致子类初始化

--- JVM/src/me/dang/chapter07/load/Test01.java
+++ JVM/src/me/dang/chapter07/load/Test01.java
@@ -3,6 +3,9 @@ package me.dang.chapter07.load;
 /**
  * 被动使用类字段演示一：
  * 通过子类引用父类的静态字段，不会导致子类初始化
+ * 对于静态字段，只有直接定义这个字段的类才会被初始化
+ * 至于是否要触发子类的加载和验证，在虚拟机规范中并未明确规定，这取决于虚拟机的具体实现
+ * 对于Sun HotSpot来说，使用-XX:+TraceClassLoading可以发现子类SubClass也是被加载了的
  * @author dht
  * @date 26/07/2019
  */
